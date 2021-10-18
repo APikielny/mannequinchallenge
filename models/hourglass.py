b226f89a9412d433ccba819de779236ab82ace9c
APikielny <apikielny@gmail.com>
2021-10-17 22:07:33 -0400

    Update hourglass.py

--- models/hourglass.py
+++ models/hourglass.py
@@ -194,7 +194,8 @@ class HourglassModel(nn.Module):
         
         if(view_feature_maps):
             # print("length: ", len(visualisation_feature_map))
-            print(visualisation_feature_map.keys())
+            # print(visualisation_feature_map.keys())
+            print(visualisation_feature_map[visualisation_feature_map.keys()[0]])
 
         return
 
