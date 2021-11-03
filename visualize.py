00f7c3b900d6540331139ad3613a96c10283c90d
Marc Mapeke <marcmapeke@gmail.com>
2021-11-02 20:04:19 -0400

    tried sklearn pca - produces nicer results

--- visualize.py
+++ visualize.py
@@ -3,84 +3,56 @@ import torch.nn as nn
 import numpy as np
 from PIL import Image
 
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import cv2
 import os
 
-# attempt to reduce dimensionality and view visualize a layer as an image
+# uses pytorch pca to visualize layer
 def visualize_layer(latent):
-
-        normalized_latent = (latent - torch.min(latent)) / torch.max(latent)
-
-        # reshaped_latent = torch.reshape(normalized_latent, (-1, 256))
+        normalized_latent = (latent - torch.min(latent)) / (torch.max(latent) - torch.min(latent))
         reshaped_latent = torch.reshape(normalized_latent, (-1, 2304))
-        # print("reshaped shape", reshaped_latent.shape)
 
-        # quick test for reshaping
-        # print("qck test")
-        # array = np.array([
-        # [[1,2], [3,4], [5,6]],
-        # [[7,8], [9,10], [11,12]]
-        # ]
-        # )
-        # array = torch.from_numpy(array)
-        # print(array.size())
-        # print(array)
-        #
-        # r_array = torch.reshape(array, (-1, 6))
-        # print(r_array.size())
-        # print(r_array)
-        #
-        # f_array = torch.reshape(r_array, (-1, 3, 2))
-        # print(f_array.size())
-        # print(f_array)
-
-        ################################### PCA
-        (U,S,V) = torch.pca_lowrank(reshaped_latent, q=256, center=False, niter=2) #TODO do I need to center?
-        # U,S,V = torch.linalg.svd(reshaped_latent)
-
+        (U,S,V) = torch.pca_lowrank(reshaped_latent, q=256, center=True, niter=2)
         k = 3
 
-        print("U shape: ", U.shape)
-        print("S shape: ", S.shape)
-        print("V shape: ", V.shape)
-
-        # projected = torch.matmul(reshaped_latent.reshape(-1, 256), U[:, :k]) #this method worked with the linalg svd #TODO I think this should be V
-        # projected = torch.matmul(reshaped_latent, V[:, :k])
+        # print("U shape: ", U.shape)
+        # print("S shape: ", S.shape)
+        # print("V shape: ", V.shape)
 
         projected = torch.matmul(reshaped_latent.T, V.T[:, :k])
-        # print("projected size: ", projected.size())
-        # 2304 x 3
 
         ch1 = torch.reshape(projected[:,0], (36, 64)).numpy()
         ch2 = torch.reshape(projected[:,1], (36, 64)).numpy()
         ch3 = torch.reshape(projected[:,2], (36, 64)).numpy()
         rgb = np.stack((ch1, ch2, ch3))
-        # print(rgb.shape)
         return rgb
 
-        #https://pytorch.org/docs/stable/generated/torch.pca_lowrank.html
-        #https://www.programcreek.com/python/example/101191/torch.svd
+# SKLEARN.DECOMP version
+# INSTALL SCIKIT: pip3 install -U scikit-learn
+def visualize_layer_sklearn(latent):
 
+        # (256, w, h)
+        print(type(latent))
+        print(latent.size())
+        # normalized_latent = (latent - torch.min(latent)) / torch.max(latent)
 
+        # reshaped_latent = torch.reshape(normalized_latent, (-1, 256))
+        # wxh = 2304
+        reshaped_latent = torch.reshape(latent, (-1, 2304))
+        # (256, wXh)
+        reshaped_latent = torch.transpose(reshaped_latent, 0, 1)
+        # (wXh, 256)
 
-        # projected = V[:,:,:3]
+        pca = PCA(n_components=3)
+        out = pca.fit_transform(reshaped_latent)
+        print(type(out))
+        print(out.shape)
 
-        # print("projected!")
+        reshaped_out = np.reshape(out, (36, 64, -1))
 
-        # normalize_projected = (projected - torch.min(projected)) / torch.max(projected)# * 0.5 #not sure why mult. by 0.5 here brings it to [0,1]ish
+        return reshaped_out
 
-        # print("normalized max and min: ", torch.max(normalize_projected), torch.min(normalize_projected))
-
-        # print(normalize_projected.shape)
-        ###
-        # print('shape: ', projected.shape)
-        # projected = torch.reshape(projected, (36, 64, -1))
-        # projected = torch.reshape(projected, (-1, 36, 64))
-
-        # return projected
-        # return normalized_latent[0,:,:]
-        # return latent[0,:,:]
 
 # view the shape of each activation layer's shape in our map
 def view_all_activation_maps(visualisation_feature_map):
@@ -109,21 +81,28 @@ def visualize(visualisation_feature_map, input_, frameName):
         latent = list(visualisation_feature_map.values())[0][0,:,:,:].cpu().detach()
         # latent = list(visualisation_feature_map.values())[1][0,:,:,:].cpu().detach()#.numpy()
 
-        latent_vis = visualize_layer(latent)
+        # SKLearn produces more consistent results
+        useSkLearn = True
 
-        # Pick Between Clipping vs Normalizing
-        # NOTE: Don't do both and normalizing looks better it seems
-        # latent_vis = latent_vis.clip(min=0)
-        # latent_vis = (latent_vis - latent_vis.min())/latent_vis.max()
+        if(useSkLearn):
+            latent_vis = visualize_layer_sklearn(latent)
+        else:
+            latent_vis = visualize_layer(latent)
 
+        # using np.ptp to max-min normalize
+        latent_vis = (latent_vis - latent_vis.min())/np.ptp(latent_vis)
         latent_vis = latent_vis * 255.0
 
-        img = cv2.merge((latent_vis[0], latent_vis[1], latent_vis[2]))
+        if useSkLearn:
+            img = cv2.merge((latent_vis[:, :, 0], latent_vis[:, :, 1], latent_vis[:, :, 2]))
+        else:
+            img = cv2.merge((latent_vis[0], latent_vis[1], latent_vis[2]))
 
         # Upscaling so a bit easier to see
         scaleFactor = 4
         img = cv2.resize(img, (64 * scaleFactor, 36 * scaleFactor))
 
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cv2.imwrite(os.path.join("latent_images", frameName + ".png"), img)
 
     return
