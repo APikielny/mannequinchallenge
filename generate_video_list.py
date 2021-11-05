9863b46701d5103fb7ea3cadaf7ede2127c38375
Marc Mapeke <marcmapeke@gmail.com>
2021-11-04 22:06:45 -0400

    temp

--- generate_video_list.py
+++ generate_video_list.py
@@ -3,11 +3,11 @@
 
 # Run by calling ./generate_video_list
 # Change directory by editing path variable
-path = "/home/adam/Desktop/repos/mannequinchallenge/test_data/custom-handheld/translate"
+path = "/home/adam/Desktop/repos/mannequinchallenge/test_data/custom-handheld/rotate"
 
 import os
 
-a = open("./test_data/test_video_list.txt", "w")
+a = open("./test_data/test_marc_video_list.txt", "w")
 
 files = os.listdir(path)
 for f in files:
