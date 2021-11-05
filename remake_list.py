812d484121da1542b0bdff3f55a67c61bc7fe0fd
Adam Pikielny <apikielny@gmail.com>
2021-11-05 14:07:27 -0400

    set single view as default input arg and change marc data to have leading zeroes

--- remake_list.py
+++ remake_list.py
@@ -4,5 +4,5 @@ path = '/home/adam/Desktop/repos/mannequinchallenge/test_data/custom-handheld/tr
 file1 = open("test_data/test_marc_video_list_reorder.txt", "a")
 num_frames = 75
 for i in range(num_frames):
-    file1.write(path + str(i) + ".jpg\n")
+    file1.write(path + str(i).zfill(4) + ".jpg\n") #pad with zeroes for sorting later
 file1.close()
