848536ff0a07134a4c46a5c7d774d930b2af832e
Adam Pikielny <apikielny@gmail.com>
2022-02-12 19:01:19 -0500

    fix batch size

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -692,7 +692,7 @@ class Pix2PixModel(base_model.BaseModel):
         self.optimizer_G.zero_grad()
         self.backward_G(1)
 
-        k = 4
+        k = 1
         if ( (i+1) % k == 0 or (i+1) == number_batches):
             self.optimizer_G.step()
             self.optimizer_G.zero_grad()
