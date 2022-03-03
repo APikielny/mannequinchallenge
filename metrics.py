5b607374a059f7821fda6056888d771434c6aeef
APikielny <apikielny@gmail.com>
2022-03-02 23:05:50 -0500

    visualizing L2 dist

--- metrics.py
+++ metrics.py
@@ -83,7 +83,7 @@ def L2_frame_consistency(folder, cut_in_half=True): # cut in half: if the frame 
         ##trying to visualize differences to see if this metric makes sense
         ####################
 
-        # cv2.imwrite("L2_frame_comparisons/visualizations/frame" + str(i) + ".jpg", np.abs(img_list[i] - img_list[i + 1]))
+        cv2.imwrite("L2_frame_comparisons/visualizations/frame" + str(i) + ".jpg", img_list[i] - img_list[i + 1])
         ##################
 
     # TODO normalize or not??
