2f8755ca0aaf8e21828fc9cdce27c60655a5c866
Adam Pikielny <apikielny@gmail.com>
2022-03-09 18:22:53 -0500

    incorporate latent weight into graph and latent weight is now a flag

--- plot_train_losses.py
+++ plot_train_losses.py
@@ -25,6 +25,6 @@ def plot_losses(latent_loss, supervision_loss, title, latent_weight = None):
     plt.savefig(graphs_folder + '/Losses_plot_' + title +'.png')
 
 # testing this file
-latent_loss = [4.1, 0.4, 0.01, 0.001]
-supervision_loss = [0.2, 0.1, 0.12, 0.09]
-plot_losses(latent_loss, supervision_loss, "test plot", 1e-1)
+# latent_loss = [4.1, 0.4, 0.01, 0.001]
+# supervision_loss = [0.2, 0.1, 0.12, 0.09]
+# plot_losses(latent_loss, supervision_loss, "test plot", 1e-1)
