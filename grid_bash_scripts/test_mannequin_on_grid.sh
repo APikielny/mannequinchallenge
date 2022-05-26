70039ab95cde145d2322ce6acbcd84a106f8ca32
Adam Pikielny <adam_pikielny@brown.edu>
2022-05-26 17:20:14 -0400

    Update test_mannequin_on_grid.sh

--- grid_bash_scripts/test_mannequin_on_grid.sh
+++ grid_bash_scripts/test_mannequin_on_grid.sh
@@ -1,9 +1,7 @@
-model_name=${1:-"0_epochs_trying_to_fix_weight_init_constant_0_weights"}
+model_name=${1:-"best_depth_Ours_Bilinear_inc_3"} #this is google's model.
 
 cd /data/jhtlab/apikieln
 source ./venv-mannequin/bin/activate
 #pip -V #check if env was activated
 cd mannequinchallenge
 python3 test_davis_videos.py --weights ${model_name}
-
-#bash L2_frame_comparison ${model_name}
